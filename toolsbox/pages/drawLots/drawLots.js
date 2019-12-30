7caec7f131604d1cac154cb8d9de63f16179196e
yehui <1248547721@qq.com>
2019-12-31 00:23:21 +0800

    抽签功能

--- toolsbox/pages/drawLots/drawLots.js
+++ toolsbox/pages/drawLots/drawLots.js
@@ -5,7 +5,72 @@ Page({
    * 页面的初始数据
    */
   data: {
+    min: 1,
+    max: 100,
+    select: 1,
+    putback: false,//默认不放回
+    result: null,
+    theme: "抽签结果如下"
+  },
 
+  themeChange: function (e) {
+    this.setData({
+      theme: e.detail.value
+    })
+  },
+  selectChange: function (e) {
+    this.setData({
+      select: e.detail.value
+    })
+  },
+  minChange: function (e) {
+    this.setData({
+      min: e.detail.value
+    })
+  },
+  maxChange: function (e) {
+    this.setData({
+      max: e.detail.value
+    })
+  },
+  switchChange: function (e) {
+    this.setData({
+      putback: e.detail.value//true不放回，false放回
+    })
+  },
+  submitForm() {
+    this.selectComponent('#form').validate((valid, errors) => {
+      console.log('valid', valid, errors)
+      if (!valid) {
+        const firstError = Object.keys(errors)
+        if (firstError.length) {
+          this.setData({
+            result: errors[firstError[0]].message
+          })
+        }
+      } else {
+        var res = [],
+          min = this.data.min,
+          max = this.data.max,
+          select = this.data.select,
+          putback = this.data.putback
+        while (select !== 0) {
+          var random = parseInt(Math.random() * (max - min + 1) + min) + 1
+          if (putback) {
+            res.push(random)
+            select -= 1
+          }
+          if (!putback && res.indexOf(random) == -1) {
+            res.push(random)
+            select -= 1
+          }
+        }
+        console.log(res)
+        this.setData({
+          result: res
+        })
+      }
+    })
   },
 
   /**
