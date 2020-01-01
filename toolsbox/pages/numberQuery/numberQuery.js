dfea8c6cbb2f3ff680e02d1666c4cc2fcb9da8ba
yehui <1248547721@qq.com>
2020-01-02 00:47:36 +0800

    添加美国的号段以及总系统图片调整

--- toolsbox/pages/numberQuery/numberQuery.js
+++ toolsbox/pages/numberQuery/numberQuery.js
@@ -11,7 +11,10 @@ Page({
     post: "",
     queryResult: false,
     msg: null,
-    disabled: true
+    disabled: true,
+    countryCodes: ["+86", "+1"],
+    region_name:["省份","州"],
+    countryCodeIndex: 0,
   },
   onLoad: function(options) {
     wx.setNavigationBarColor({
@@ -19,17 +22,31 @@ Page({
       backgroundColor: "#00b26a",
     })
   },
+
+  onCountryCodeChange: function(e) {
+    this.setData({
+      countryCodeIndex: e.detail.value
+    })
+  },
+
   onInputChange: function(e) {
-    if (e.detail.value.length !== 11) {
+    if (this.data.countryCodeIndex==0 && e.detail.value.length !== 11) {
       this.setData({
         msg: "输入提示：手机号长度应为11位数字",
         queryResult: false,
         disabled: true
       })
-    } else {
+    } else if (this.data.countryCodeIndex ==1 && e.detail.value.length !== 10){
+      this.setData({
+        msg: "输入提示：手机号长度应为10位数字",
+        queryResult: false,
+        disabled: true
+      })
+    } 
+    else {
       this.setData({
         msg: null,
-        number: parseInt(e.detail.value.substr(0, 7)), //得到号码的前七位
+        number: e.detail.value, //得到号码的前七位
         disabled: false,
       })
     }
@@ -39,155 +56,60 @@ Page({
     const db = wx.cloud.database()
     // 查询当前用户所有的 counters
     console.log("clicked query")
-    db.collection('numbers').where({
-      ndc_sn_from: this.data.number
-    }).get({
-      success: res => {
-        this.setData({
-          queryResult: true,
-          city: res.data[0].city,
-          region: res.data[0].origin,
-          operator: res.data[0].operator,
-          area: res.data[0].city_code,
-          post: res.data[0].post_code,
+    if(this.data.countryCodeIndex == 0){
+      db.collection('numbers').where({
+        ndc_sn_from: parseInt(this.data.number.substr(0, 7))
+      }).get({
+        success: res => {
+          this.setData({
+            queryResult: true,
+            city: res.data[0].city,
+            region: res.data[0].origin,
+            operator: res.data[0].operator,
+            area: res.data[0].city_code,
+            post: res.data[0].post_code,
+          })
+          console.log('[数据库] [查询记录] 成功: ', res)
+        },
+        fail: err => {
+          wx.showToast({
+            icon: 'none',
+            title: '查询记录失败'
+          })
+          console.error('[数据库] [查询记录] 失败：', err)
+        }
+      })
+    }else{
+      const _ = db.command
+      const start = parseInt(this.data.number.substr(3, 4))
+      // console.log(start*1000)
+      db.collection('usa_numbers').where(
+        {
+          ndc: parseInt(this.data.number.substr(0, 3)),
+          ndc_sn_from:start*1000
+        }
+      ).get({
+        success: res => {
+          console.log(res)
+          this.setData({
+            queryResult: true,
+            city: res.data[0].city,
+            region: res.data[0].region,
+            operator: res.data[0].operator,
+            area: "",
+            post: "",
+          })
+          console.log('[数据库] [查询记录] 成功: ', res)
+        },
+        fail: err => {
+          wx.showToast({
+            icon: 'none',
+            title: '查询记录失败'
+          })
+          console.error('[数据库] [查询记录] 失败：', err)
+        }
         })
-        console.log('[数据库] [查询记录] 成功: ', res)
-      },
-      fail: err => {
-        wx.showToast({
-          icon: 'none',
-          title: '查询记录失败'
-        })
-        console.error('[数据库] [查询记录] 失败：', err)
-      }
-    })
+    }
+    
   }
-
-
-
-
-  //   /**
-  //    * 页面的初始数据
-  //    */
-  //   data: {
-
-  //   },
-
-  //   /**
-  //    * 生命周期函数--监听页面加载
-  //    */
-
-
-  //   /**
-  //    * 生命周期函数--监听页面初次渲染完成
-  //    */
-  //   onReady: function () {
-
-  //   },
-
-  //   /**
-  //    * 生命周期函数--监听页面显示
-  //    */
-  //   onShow: function () {
-
-  //   },
-
-  //   /**
-  //    * 生命周期函数--监听页面隐藏
-  //    */
-  //   onHide: function () {
-
-  //   },
-
-  //   /**
-  //    * 生命周期函数--监听页面卸载
-  //    */
-  //   onUnload: function () {
-
-  //   },
-
-  //   /**
-  //    * 页面相关事件处理函数--监听用户下拉动作
-  //    */
-  //   onPullDownRefresh: function () {
-
-  //   },
-
-  //   /**
-  //    * 页面上拉触底事件的处理函数
-  //    */
-  //   onReachBottom: function () {
-
-  //   },
-
-  //   /**
-  //    * 用户点击右上角分享
-  //    */
-  //   onShareAppMessage: function () {
-
-  //   }
-  // })({
-
-  //   /**
-  //    * 页面的初始数据
-  //    */
-  //   data: {
-
-  //   },
-
-  //   /**
-  //    * 生命周期函数--监听页面加载
-  //    */
-  //   onLoad: function (options) {
-
-  //   },
-
-  //   /**
-  //    * 生命周期函数--监听页面初次渲染完成
-  //    */
-  //   onReady: function () {
-
-  //   },
-
-  //   /**
-  //    * 生命周期函数--监听页面显示
-  //    */
-  //   onShow: function () {
-
-  //   },
-
-  //   /**
-  //    * 生命周期函数--监听页面隐藏
-  //    */
-  //   onHide: function () {
-
-  //   },
-
-  //   /**
-  //    * 生命周期函数--监听页面卸载
-  //    */
-  //   onUnload: function () {
-
-  //   },
-
-  //   /**
-  //    * 页面相关事件处理函数--监听用户下拉动作
-  //    */
-  //   onPullDownRefresh: function () {
-
-  //   },
-
-  //   /**
-  //    * 页面上拉触底事件的处理函数
-  //    */
-  //   onReachBottom: function () {
-
-  //   },
-
-  //   /**
-  //    * 用户点击右上角分享
-  //    */
-  //   onShareAppMessage: function () {
-
-  //   }
 })
