a1c4c005c0268cd75945664d95ce64b967918941
yehui <1248547721@qq.com>
2020-01-01 23:20:24 +0800

    各个界面的完善与修改

--- toolsbox/pages/drawLots/drawLots.js
+++ toolsbox/pages/drawLots/drawLots.js
@@ -86,7 +86,10 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.setNavigationBarColor({
+      frontColor: '#ffffff',
+      backgroundColor: '#ef9f91',
+    })
   },
 
   /**
