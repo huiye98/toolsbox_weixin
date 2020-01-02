e33cbd7193b53f4244772fecc1772432a06cbfc5
yehui <1248547721@qq.com>
2020-01-02 09:49:00 +0800

    完善文件结构

--- toolsbox/pages/openapi/openapi.js
+++ toolsbox/pages/openapi/openapi.js
@@ -1,45 +1,8 @@
 Page({
 
   data: {
-    templateMessageResult: '',
     wxacodeSrc: '',
     wxacodeResult: '',
-    showClearWXACodeCache: false,
-  },
-
-  submitTemplateMessageForm(e) {
-    this.setData({
-      templateMessageResult: '',
-    })
-
-    wx.cloud.callFunction({
-      name: 'openapi',
-      data: {
-        action: 'sendTemplateMessage',
-        formId: e.detail.formId,
-      },
-      success: res => {
-        console.warn('[云函数] [openapi] templateMessage.send 调用成功：', res)
-        wx.showModal({
-          title: '发送成功',
-          content: '请返回微信主界面查看',
-          showCancel: false,
-        })
-        wx.showToast({
-          title: '发送成功，请返回微信主界面查看',
-        })
-        this.setData({
-          templateMessageResult: JSON.stringify(res.result)
-        })
-      },
-      fail: err => {
-        wx.showToast({
-          icon: 'none',
-          title: '调用失败',
-        })
-        console.error('[云函数] [openapi] templateMessage.send 调用失败：', err)
-      }
-    })
   },
 
   onGetWXACode() {
@@ -47,21 +10,17 @@ Page({
     this.setData({
       wxacodeSrc: '',
       wxacodeResult: '',
-      showClearWXACodeCache: false,
     })
 
-    // 此处为演示，将使用 localStorage 缓存，正常开发中文件 ID 应存在数据库中
-    const fileID = wx.getStorageSync('wxacodeCloudID')
+    const fileID = "cloud://toolslbox-ymyjc.746f-toolslbox-ymyjc-1301022209/wxacode_default_openapi_page.jpeg"
 
     if (fileID) {
       // 有云文件 ID 缓存，直接使用该 ID
-      // 如需清除缓存，选择菜单栏中的 “工具 -> 清除缓存 -> 清除数据缓存”，或在 Storage 面板中删掉相应的 key
       this.setData({
         wxacodeSrc: fileID,
-        wxacodeResult: `从本地缓存中取得了小程序码的云文件 ID`,
-        showClearWXACodeCache: true,
+        wxacodeResult: `取得了小程序码的云文件 ID`,
       })
-      console.log(`从本地缓存中取得了小程序码的云文件 ID：${fileID}`)
+      console.log(`取得了小程序码的云文件 ID：${fileID}`)
     } else {
       wx.cloud.callFunction({
         name: 'openapi',
@@ -76,7 +35,6 @@ Page({
           this.setData({
             wxacodeSrc: res.result,
             wxacodeResult: `云函数获取二维码成功`,
-            showClearWXACodeCache: true,
           })
           wx.setStorageSync('wxacodeCloudID', res.result)
         },
@@ -89,21 +47,6 @@ Page({
         }
       })
     }
-  },
-
-  clearWXACodeCache() {
-    wx.removeStorageSync('wxacodeCloudID')
-
-    this.setData({
-      wxacodeSrc: '',
-      wxacodeResult: '',
-      showClearWXACodeCache: false,
-    })
-
-    wx.showToast({
-      title: '清除成功',
-    })
-  },
-
+  }
 })
 
